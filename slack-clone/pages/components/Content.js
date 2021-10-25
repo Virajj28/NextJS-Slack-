5b9f3462ba2915a60cd69cdd0f03f8cecd499dc7
Viraj Jadhav <viraj28j@gmail.com>
2021-10-25 18:09:32 +0530

    components passed in dashboard

---
+++ slack-clone/pages/components/Content.js
@@ -0,0 +1,5 @@
+export default function Content() {
+    return(
+        <h1 class="text-center mt-2 font-bold">Testing D3.js in dashboard</h1>
+    )
+}
---
+++ slack-clone/pages/components/LeftNavbar.js
@@ -0,0 +1,11 @@
+export default function LeftNavbar() {
+    return (
+            <div class="container bg-gray-200 text-center">
+                <ul>
+                    <li><a href="./dashboard">Dashboard</a></li>
+                    <li><a href="/">Register</a></li>
+                    <li><a href="./login">login</a></li>
+                </ul>
+            </div>
+    )
+}
