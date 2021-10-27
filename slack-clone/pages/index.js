e09ace21dba82a83223d94f6d8c32c91486368c0
Viraj Jadhav <viraj28j@gmail.com>
2021-10-27 15:57:00 +0530

    login auth0 done

--- slack-clone/pages/index.js
+++ slack-clone/pages/index.js
@@ -9,14 +9,16 @@ export default function Home() {
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
 
-  if (user) {
-    return (
+  if(user) {
+  return (
       <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <a href="/api/auth/logout">Logout</a>
       </div>
     );
-  }
-
+}
 
   return (
     <div>
@@ -26,13 +28,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <ul>
-        <li>
           <Link href="/api/auth/login">
             <a>Login</a>
           </Link>
-        </li>
-      </ul>
 
       <div class="grid place-items-center mt-48">
         <div class="text-center">
